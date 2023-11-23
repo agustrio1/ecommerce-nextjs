b76606070359603b0fc59242d9c25d90b0165a46
agustrio1 <agustrio1998@gmail.com>
2023-11-24 06:47:49 +0700

    first commit

---
+++ app/addproduct/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Productform from './productForm'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <Productform/>
+  )
+}
+
+export default page
---
+++ app/signup/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SignForm from './signForm'
+
+function page() {
+  return (
+    <SignForm/>
+  )
+}
+
+export default page
